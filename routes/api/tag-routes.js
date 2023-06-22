fd661b80b67dd3359221291d274707842fd14616
Chartok <m.bhimjee@outlook.com>
2023-06-22 00:20:50 -0500

    refactored product/tag-route.js using async/await

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -18,26 +18,79 @@ router.get('/', async (req, res) => {
     });
     res.json(products);
   } catch (err) {
-    console.log(err);
+    console.log('There was an error retrieving the products', err);
     res.status(500).json(err);
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try{
+    const product = await Product.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [
+        Category,
+        {
+          model: Tag,
+          through: ProductTag,
+        },
+      ],
+    });
+    res.json(product);
+  } catch (err) {
+    console.log('There was an error retrieving the product', err);
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try{
+    const product = await Product.create({
+      product_name: req.body.product_name,
+      price: req.body.price,
+      stock: req.body.stock,
+      backorder: req.body.backorder,
+      category_id: req.body.category_id,
+    });
+    res.json(product);
+  } catch (err) {
+    console.log('There was an error creating the product', err);
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const product = await Product.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    res.json(product);
+  } catch (err) {
+    console.log('There was an error updating the product', err);
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const product = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.json(product);
+  } catch (err) {
+    console.log('There was an error deleting the product', err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
