4e0a98c45278381f42654b6d7fb4f23bf0514ff9
Chartok <m.bhimjee@outlook.com>
2023-06-22 01:25:19 -0500

    finished coding routes; category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,7 +5,6 @@ const { Category, Product } = require('../../models');
 
 router.get('/', async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
   try {
     const categories = await Category.findAll({
       include: [Product],
@@ -13,18 +12,28 @@ router.get('/', async (req, res) => {
 
     res.json(categories);
 
+    console.log('Here are all of the categories')
   } catch (err) {
     console.log('There was an error retrieving the categories', err);
     res.status(500).json(err);
+  }
 });
 
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
   try {
 
+    const category = await Category.findByPk(req.params.id, {
+      where: {
+        id: req.params.id,
+      },
+      include: [Product]
+    })
+    res.json(category);
+
+    console.log('Category retrieved')
   } catch (err) {
-    console.log('There was an error', err)
+    console.log('There was an error retrieving the category', err)
     res.status(500).json(err);
   }
 });
@@ -33,8 +42,14 @@ router.post('/', async (req, res) => {
   // create a new category
   try {
 
+    const category = await Category.create({
+      category_name: req.body.category_name,
+    })
+    res.json(category);
+
+    console.log('Category created')
   } catch (err) {
-    console.log('There was an error', err)
+    console.log('There was an error there was an error creating the category', err)
     res.status(500).json(err);
   }
 });
@@ -43,8 +58,16 @@ router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
 
+    const category = await Category.update({
+      where: {
+        id: req.params.id,
+      },
+    })
+    res.json(category);
+
+    console.log('Category updated')
   } catch (err) {
-    console.log('There was an error', err)
+    console.log('There was an error there was an error updating the category', err)
     res.status(500).json(err);
   }
 });
@@ -53,8 +76,17 @@ router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
 
+    const category = await Category.destroy({
+      where: {
+        id: req.params.id,
+      }
+    })
+    res.json(category)
+
+    
+    console.log('Category deleted')
   } catch (err) {
-    console.log('There was an error', err)
+    console.log('There was an error deleting the category', err)
     res.status(500).json(err);
   }
 });
