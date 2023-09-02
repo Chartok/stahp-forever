218f6d788328165646208aaad3aefa70c0bcec2c
Chartok <m.bhimjee@outlook.com>
2023-09-02 08:45:37 -0500

    added tag columns to tag model, added cors option to express server, updated dependencies

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
-// import sequelize connection
 
+// import sequelize connection
+const { Sequelize } = require('sequelize');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+let corsOptions = {
+  origin: 'http://localhost 3001'
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
+
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+  console.log(`App listening on local host port http://localhost:${PORT}!`);
 });
